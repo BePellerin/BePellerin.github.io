dfff4823ead010ced1f23105eb3b2aff956a5474
BePellerin <bepellerin@gmail.com>
2023-06-28 13:14:21 +0200

    modifications finales 02

--- srcGrid/app.js
+++ srcGrid/app.js
@@ -17,3 +17,59 @@ allGridItems.forEach((el) => el.addEventListener("click", openPopup));
 
 popupImg.addEventListener("click", (e) => e.stopPropagation());
 popupBg.addEventListener("click", closePopup);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".grid-img");
+  let currentIndex = 0;
+
+
+  function showPopup(imageIndex) {
+    const popupBg = document.getElementById("popup-bg");
+    const popupContent = document.getElementById("popup-content");
+    const popupImg = document.getElementById("popup-img");
+    const closeBtn = document.getElementById("popup-close");
+
+    // Afficher l'image en plein écran
+    popupImg.src = images[imageIndex].src;
+    popupBg.style.display = "flex";
+
+    // Fermer l'image en plein écran lors du clic sur le bouton de fermeture
+    closeBtn.addEventListener("click", function () {
+      popupBg.style.display = "none";
+    });
+  }
+
+  // Fonction pour passer à l'image précédente
+  function goToPreviousImage() {
+    currentIndex--;
+    if (currentIndex < 0) {
+      currentIndex = images.length - 1;
+    }
+    showPopup(currentIndex);
+  }
+
+  // Fonction pour passer à l'image suivante
+  function goToNextImage() {
+    currentIndex++;
+    if (currentIndex >= images.length) {
+      currentIndex = 0;
+    }
+    showPopup(currentIndex);
+  }
+
+  // Écouter les événements de touche
+  document.addEventListener("keydown", function (event) {
+    if (event.keyCode === 37) { // Flèche gauche
+      goToPreviousImage();
+    } else if (event.keyCode === 39) { // Flèche droite
+      goToNextImage();
+    }
+  });
+
+  // Écouter les clics sur les images
+  images.forEach(function (image, index) {
+    image.addEventListener("click", function () {
+      showPopup(index);
+    });
+  });
+});
